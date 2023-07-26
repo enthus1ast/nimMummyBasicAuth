9dbd728cf33b354ed12d06177ee3eea995358532
David Krause (enthus1ast) <krause@biochem2.uni-frankfurt.de>
2023-07-26 14:02:05 +0200

    nim pretty

--- src/mummyBasicAuth.nim
+++ src/mummyBasicAuth.nim
@@ -10,13 +10,15 @@ proc splitBasicAuth(val: string): tuple[user, pass: string] =
     if val.len <= basic.len: return
     if not val.startsWith(basic): return
     let deco = val[basic.len .. ^1].decode()
-    let pos = parseUntil(deco, result.user, ':',  0) + 1
+    let pos = parseUntil(deco, result.user, ':', 0) + 1
     result.pass = deco[pos .. ^1]
   except:
     return
 
-proc requiresAuth*(handler: RequestHandler, testProc: TestBasicAuth, realm = "Login", failureBody = "please log in", headers: HttpHeaders = emptyHttpHeaders()): RequestHandler =
- return proc (request: Request) =
+proc requiresAuth*(handler: RequestHandler, testProc: TestBasicAuth,
+    realm = "Login", failureBody = "please log in",
+    headers: HttpHeaders = emptyHttpHeaders()): RequestHandler =
+  return proc (request: Request) =
     if "Authorization" in request.headers:
       let (user, pass) = splitBasicAuth(request.headers["Authorization"])
       if testProc(user, pass):
